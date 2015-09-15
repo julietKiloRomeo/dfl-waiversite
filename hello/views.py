b86a387cd93c9f68d9ea7eaf9429933b7a79f472
Jimmy <jimmy.kjaersgaard@gmail.com>
2015-09-15 20:17:24 +0200

    first real commit

--- hello/views.py
+++ hello/views.py
@@ -3,9 +3,10 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
+import os
+
 # Create your views here.
 def index(request):
-    # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
 
 
