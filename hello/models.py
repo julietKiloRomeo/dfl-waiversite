2e7649476e51926a6b5ac7b1cf0acc3d26f2c22b
Jimmy <jimmy.kjaersgaard@gmail.com>
2015-09-28 14:26:00 +0200

    adding util.py

--- hello/models.py
+++ hello/models.py
@@ -5,20 +5,30 @@ from django.contrib.auth.models import User
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
 
-
 class Team(models.Model):
     name    = models.CharField(max_length=100)
     account = models.IntegerField()
-    owner   = models.ForeignKey(User, unique=True)
+    owner   = models.OneToOneField(User)
+    nfl_id  = models.IntegerField(unique=True)
+    def __unicode__(self):
+        return self.name
 
 class Bid(models.Model):
     team    = models.ForeignKey('Team')
     amount  = models.IntegerField()
     player  = models.ForeignKey('Player')
+    drop    = models.ForeignKey('Player', related_name='to_drop', null=True)
     date    = models.DateTimeField('date placed', auto_now_add=True)
+    processed = models.BooleanField(default=False)
+    def __unicode__(self):
+        return '%20s : %s' % (self.team.name, self.player.name)
+        
 
 class Player(models.Model):
-    name        = models.CharField()
-    nflteam     = models.CharField()
-    dflteam     = models.ForeignKey('Team')
+    name        = models.CharField(max_length=100)
+    nflteam     = models.CharField(max_length=100, null=True)
+    dflteam     = models.ForeignKey('Team', null=True)
+    nfl_id      = models.IntegerField(unique=True)
+    def __unicode__(self):
+        return self.name
     
--- hello/views.py
+++ hello/views.py
@@ -4,17 +4,17 @@ from django.http import HttpResponseRedirect
 from .models import Greeting
 import util
 from django.contrib import auth
+from models import Player, Team, Bid
 
 #import os
 
 # Create your views here.
 def index(request):
     trades = util.latest_trades()
-    return render(request, 'index.html', {'trades': trades})
-
+    N_bids = len(Bid.objects.filter(processed=False))
+    return render(request, 'index.html', {'trades': trades, 'N':N_bids})
 
 def db(request):
-
     greeting = Greeting()
     greeting.save()
 
@@ -22,12 +22,48 @@ def db(request):
 
     return render(request, 'db.html', {'greetings': greetings})
 
-def bid(request):
-    return render(request, 'index.html', {'trades': ['bid']})
+def bid(request, nfl_id):
+    u       = request.user
+    if u.is_authenticated():
+        p       = Player.objects.get(nfl_id=nfl_id)
+        if request.method == 'POST':
+            team    = Team.objects.get(owner=u)
+            val     = int(request.POST.get('bidvalue'))
+            b = Bid(team=team, amount=val, player=p)
+            b.save()
+            return HttpResponseRedirect("/team")
+        else:
+            return render(request, 'bid.html', {'player': p})
+    else:
+        return HttpResponseRedirect("/login")
+
+def team(request):
+    u       = request.user
+    if u.is_authenticated():
+        team    = Team.objects.get(owner=u)
+        bids    = Bid.objects.filter(team=team)
+        return render(request, 'team.html', {'user': request.user, 'team':team, 'bids':bids})
+    else:
+        return HttpResponseRedirect("/login")
+    
+
+def search(request):
+    u       = request.user
+    if u.is_authenticated():
+        if request.method == 'POST':
+            if 'Search_NFL' in request.POST:        
+                util.add_from_search(request.POST.get('playername'))
+            matches = Player.objects.filter(name__icontains = request.POST.get('playername'))
+        else:
+            matches = []
+    
+        return render(request, 'search.html', {'matches': matches})
+    else:
+        return HttpResponseRedirect("/login")
+
 
 def rules(request):
-    return render(request, 'index.html', {'trades': ['150$']})
-
+    return render(request, 'rules.html')
 
 
 def login(request):
@@ -42,3 +78,24 @@ def login(request):
     else:
         # Show an error page
         return HttpResponseRedirect("/invalid/")
+        
+        
+        
+def week_results(request):
+    current_bids        = Bid.objects.filter(processed=False)
+
+    bids    = {}    
+    winners = {}    
+    for b in current_bids:
+        player = b.player.nfl_id
+        bids.setdefault(player , []).append([b.amount, b, False])
+    
+    for p in bids.keys():
+        sorted_bids = sorted(bids[p], key=lambda x:x[0], reverse=True)
+        bids[p] = sorted_bids
+        winner = util.bid_winner(bids[p])
+        winners[p]=winner
+
+    return render(request, 'results.html', {'bids':bids,'winners':winners})
+
+    
