aae745809753f57e46d5036329edd0f6324da2d9
Jimmy <jimmy.kjaersgaard@gmail.com>
2015-12-13 13:47:59 +0100

    changed search view to include team and position in results

--- hello/admin.py
+++ hello/admin.py
@@ -5,17 +5,19 @@ from django.contrib import admin
 from .models import Team, Bid, Player
 
 
-def make_swapped(admin, req, queryset):
-    queryset.update(swapped_on_nfl=True)
-make_swapped.short_description = 'Set as swapped on NFL'
-def make_processed(admin, req, queryset):
-    queryset.update(processed=True)
-make_processed.short_description = 'Set as processed'
 
 class BidsAdmin(admin.ModelAdmin):
     list_display = ('player', 'team', 'drop', 'priority', 'processed','succesful','swapped_on_nfl','date')
     list_filter  = ['succesful', 'processed', 'swapped_on_nfl']
-    actions      = [make_swapped, make_processed]
+    actions      = ['make_swapped', 'make_processed']
+
+    def make_swapped(admin, req, queryset):
+        queryset.update(swapped_on_nfl=True)
+    make_swapped.short_description = 'Set as swapped on NFL'
+    def make_processed(admin, req, queryset):
+        queryset.update(processed=True)
+    make_processed.short_description = 'Set as processed'
+
 
 class PlayerAdmin(admin.ModelAdmin):
     list_display = ('name', 'dflteam', 'position')
